0126065e3ebbbb7971b4f256b174e4f53e991f41
Molly Olson <molly.olson@vanderbilt.edu>
2019-01-16 15:24:37 -0600

    awesome local edit

--- test.R
+++ test.R
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 #' ---
 #' output: github_document
 #' ---
 
 print("hello world")
-=======
-print("hello world")
 
-x <- 1:10
->>>>>>> a521d9d... made a change in past branch so I can cherry pick
+
+require(tgsify)
