3efc5146ba6656eb5231d07695f93f40defc9b01
Molly Olson <molly.olson@vanderbilt.edu>
2019-01-16 15:07:56 -0600

    committing a cherry pick

--- test.R
+++ test.R
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 #' ---
 #' output: github_document
 #' ---
 
 print("hello world")
+=======
+print("hello world")
+
+x <- 1:10
+>>>>>>> a521d9d... made a change in past branch so I can cherry pick
